52bd3c5a5f47a07ff251888d74644dff29349400
Vinicius Moreira <viniciuspgmoreira@hotmail.com>
2021-03-24 17:48:16 -0300

    Added firebase and did the necessary configuration to access our project on firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTUZHwnZv1tzPdzOujyfFtaxP_dMVhUPE",
+    authDomain: "eventos-77484.firebaseapp.com",
+    databaseURL: "https://eventos-77484.firebaseio.com",
+    projectId: "eventos-77484",
+    storageBucket: "eventos-77484.appspot.com",
+    messagingSenderId: "684203085448",
+    appId: "1:684203085448:web:9e5a4ce6a7d8c45c67193d",
+    measurementId: "G-EH0B54CQRG"
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
